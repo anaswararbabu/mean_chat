c1a044419e10028a26551b3b2340d82d776d497d
anaswararbabu <74505032+anaswararbabu@users.noreply.github.com>
2021-04-01 20:32:57 +0530

    Update Messagedata.js

--- Backend/src/model/Messagedata.js
+++ Backend/src/model/Messagedata.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://userone:userone@cluster0.akfoz.mongodb.net/chatapp?retryWrites=true&w=majority');
 mongoose.connect('mongodb://localhost:27017/ChatingApp'); 
-    // { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex : true, useFindAndModify: false });
+   
 
 //Schema
 const Schema = mongoose.Schema;
@@ -11,7 +10,7 @@ const MessageSchema =  new Schema({
     from: String,
     message:String,
     image:String,
-    // date:Date,
+  
     date:{type:Date , default:Date.now},
     room:String
 });
